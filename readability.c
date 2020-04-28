eddbf97147625a7920ac4922a71f8bb3e945012f
razahaider03 <razahaider03@gmail.com>
2020-04-28 14:09:32 +0000

    version2.0

--- readability.c
+++ readability.c
@@ -10,24 +10,34 @@ int main (void)
     {
         printf("Your Text is : %s\n", text);
     }
-    for(p=0,r=strlen(text); p<r; p++)
-    {
+       for(p=0,r=strlen(text); p<r; p++)
+        {
         // for counting Letters.
         if (text[p]!= ' ' && text[p]!= '\0' && text[p]!='-' && text[p]!='_' && text[p]!= '.')
         {
             q++;
         }
+        }
         // for counting Words.
-        else if (text[p]==' ' || text[p]== '-' || text[p]== '\0'  || text[p]=='_')
+        for(p=0,r=strlen(text); p<r; p++)
+        {
+        if (text[p]==' ' || text[p]== '-' || text[p]== '\0'  || text[p]=='_'|| text[p]=='!')
         {
             j++;
         }
+        }
         // for counting Sentences.
-        else if (text[p]== '.' || text[p]== '!' || text[p]== '\0')
+        for(p=0,r=strlen(text); p<r; p++)
+        {
+        if (text[p]== '.' || text[p]== '!' || text[p]== '\0')
         {
             k++;
         }
-    }
+        }
+        if (q>0 && text[strlen(text)-1]!='!' && text[strlen(text)-1]!= '.' && text [strlen(text)-1]!=' ')
+	   {
+	        k++;
+       }
         printf("no.of chars is %i\n",q);
         printf("no.of words is %i\n",j);
         printf("no.of sentences is %i\n",k);
