563da0dee84804596e0ed2bbc690dcb027974563
razahaider03 <razahaider03@gmail.com>
2020-04-28 05:30:51 +0000

    completed_1.0

--- readability.c
+++ readability.c
@@ -6,20 +6,30 @@
 int main (void)
 {
     string text = get_string("text: ");
-    int p=0,q=0,r;
-
+    int p=0,q=0,r,j=0,k=0;
     {
         printf("Your Text is : %s\n", text);
     }
-    for(p=0,r=strlen(text);p<r;p++)
+    for(p=0,r=strlen(text); p<r; p++)
     {
-        if (text[p]!= ' ' && text[p]!= '\0')
+        // for counting Letters.
+        if (text[p]!= ' ' && text[p]!= '\0' && text[p]!='-' && text[p]!='_' && text[p]!= '.')
         {
             q++;
         }
-
+        // for counting Words.
+        else if (text[p]==' ' || text[p]== '-' || text[p]== '\0'  || text[p]=='_')
+        {
+            j++;
+        }
+        // for counting Sentences.
+        else if (text[p]== '.' || text[p]== '!' || text[p]== '\0')
+        {
+            k++;
+        }
     }
-    printf("no.of char is %i\n",q);
-    printf("are u happy");
+        printf("no.of chars is %i\n",q);
+        printf("no.of words is %i\n",j);
+        printf("no.of sentences is %i\n",k);
 }
 
